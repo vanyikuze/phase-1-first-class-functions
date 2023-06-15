4251ba678b29cf7c76e6ede83e4f8f6e244bc52f
Vany <vany.ikuze@student.moringaschool.com>
2023-06-15 12:27:52 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    callback()
+}
+
+function returnsANamedFunction(){
+    return function name(){}
+}
+
+function returnsAnAnonymousFunction(){
+ 
+    return function (){}
+}
